576365dbeef247f68b8d415a4d348d0b8a7c1763
Bailey Andrew <e.bailey.andrew@gmail.com>
2020-08-04 23:19:25 +0100

    0.2a Alpha

--- MagicNumbers.py
+++ MagicNumbers.py
@@ -6,4 +6,4 @@ All global constants are here
 
 DRAW_VERTICES = True
 CLOSENESS_CONSTANT = 1 # how close two tiles have to be to be considered the same
-VERSION = "0.1a [Alpha]"
+VERSION = "0.2a [Alpha]"
