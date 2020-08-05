501c9aed0c3f82ad296b55a0f835d45bc06fa776
Bailey Andrew <e.bailey.andrew@gmail.com>
2020-08-05 13:28:54 +0100

    0.2b Alpha

--- MagicNumbers.py
+++ MagicNumbers.py
@@ -7,4 +7,4 @@ All global constants are here
 DRAW_VERTICES = True
 LENIENT = True # Some things that fail asserts or raise NotImplementedErrors will fail gracefully
 CLOSENESS_CONSTANT = 1 # how close two tiles have to be to be considered the same
-VERSION = "0.2a [Alpha]"
+VERSION = "0.2b [Alpha]"
